813d406bff3394cfc60f039ec4f7c9cabd2f96b1
FranAstarloa <astarloafrancisco@gmail.com>
2021-05-25 13:47:45 -0300

    detalle de producto

--- website/app.js
+++ website/app.js
@@ -6,5 +6,8 @@ const app = express();
 app.listen(3030, ()=>console.log("server start in http://localhost:3030"));
 
 app.use(express.static(path.resolve(__dirname, "./public")));
-
+/*home*/
 app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname, "./views", "index.html")));
+
+/*product detail*/
+app.get("/product",(req,res)=> res.sendFile(path.resolve(__dirname, "./views", "productDetail.html")));
