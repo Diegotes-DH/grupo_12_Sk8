26dc7072094d3ad6edb024d9d348f255896d06c2
Diegotes-HD <testadiegom@gmail.com>
2021-05-30 11:10:08 -0300

    update

--- website/app.js
+++ website/app.js
@@ -8,3 +8,7 @@ app.listen(3030, ()=>console.log("server start in http://localhost:3030"));
 app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname, "./views", "index.html")));
+
+app.get("/register",(req,res)=> res.sendFile(path.resolve(__dirname, "./views", "register.html")));
+
+app.get("/login",(req,res)=> res.sendFile(path.resolve(__dirname, "./views", "login.html")));
