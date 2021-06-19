39df2ea4b1f0090387c511b95c2859f189e6ccd0
Diegotes-HD <testadiegom@gmail.com>
2021-06-19 09:40:07 -0300

    update sprint 3

--- website/src/controllers/mainController.js
+++ website/src/controllers/mainController.js
@@ -1,8 +1,8 @@
 const controller = {
     home:(req,res) => res.render("index"),
-    register: (req,res)=> res.render("register"),
-    login: (req,res)=> res.render("login"),
-    detail: (req,res)=> res.render("productDetail"),
+    register: (req,res)=> res.render("users/register"),
+    login: (req,res)=> res.render("users/login"),
+    detail: (req,res)=> res.render("products/productDetail"),
     cart: (req,res)=> res.render("cart")
 }
 module.exports = controller;
